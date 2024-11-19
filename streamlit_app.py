91426069daf37a72b8859343becfe01e4bb0a904
snobsl <snobslam@gmail.com>
2024-11-20 05:16:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
+# Judul aplikasi
 st.title("🎈 Sentiment Analysis Application")
+
+# Deskripsi aplikasi
 st.write(
     "Selamat datang di aplikasi sentimen analisis!"
 )
+
+# Input teks dari pengguna
+user_input = st.text_area("Enter text for sentiment analysis:")
